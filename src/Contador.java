31dbcaead0c8b7fbe4deb8415f3f5a624afa56b2
Kethlyn <kethdiniz06@gmail.com>
2023-07-20 13:47:10 -0300

    inserindo novo desafio

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int primeiroNumero = terminal.nextInt();
+        System.out.println("Digite outro número: ");
+        int segundoNumero = terminal.nextInt();
+
+        try {
+            for(int i = 0; i <= contar(primeiroNumero, segundoNumero); i ++){
+                System.out.println("Imprimindo o número " + i);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Número não pode dar negativo.");
+        }
+    }
+
+    static int contar(int primeiroNumero, int segundoNumero) throws IllegalArgumentException {
+        if (segundoNumero < primeiroNumero){
+            throw new IllegalArgumentException();
+        } else {
+            return segundoNumero - primeiroNumero;
+        }
+    }
+
+
+}
+
